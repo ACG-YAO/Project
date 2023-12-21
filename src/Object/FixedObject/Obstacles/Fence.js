666982273ea8baa05906eff5b31a4f0fda63f4dc
DrM-RL <drm.visualrl@gmail.com>
2023-12-21 13:03:03 +0800

    add_sky

--- src/Object/FixedObject/Obstacles/Fence.js
+++ src/Object/FixedObject/Obstacles/Fence.js
@@ -1,13 +1,29 @@
 import { Obstacles } from './Obstacles.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import * as THREE from 'three';
+
+const textureLoader = new THREE.TextureLoader();
+const baseColorTexture = textureLoader.load('Textures/WoodFence_baseColor.png');
+const metallicRoughnessTexture = textureLoader.load('Textures/WoodFence_metallicRoughness.png');
+const normalTexture = textureLoader.load('Textures/WoodFence_normal.png');
 
 export class Fence extends Obstacles {
     constructor(scene, onLoadCallback) {
         super();
         this.promise = new Promise((resolve, reject) => {
             const loader = new GLTFLoader();
-            loader.load('Models/Fence.glb', (gltf) => {
-                this.initialize(gltf, scene, onLoadCallback, 8, 3, 2);
+            loader.load('Models/Fence.gltf', (gltf) => {
+                gltf.scene.traverse((child) => {
+                    if (child.isMesh) {
+                        child.material.map = baseColorTexture;
+                        child.material.metalnessMap = metallicRoughnessTexture; 
+                        child.material.roughnessMap = metallicRoughnessTexture;
+                        child.material.normalMap = normalTexture;
+                        child.material.needsUpdate = true;
+                    }
+                });
+
+                this.initialize(gltf, scene, onLoadCallback, 2.67, 3, 1);
                 resolve(gltf);
             }, undefined, (error) => {
                 reject(error);
